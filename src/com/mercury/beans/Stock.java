4ee2deb0ea877be0be6473cc01fd4cae8701d4ba
yxlink <yxlink@gmail.com>
2015-09-22 14:34:54 -0400

    Changed Hibernate mapping to annotation

---
+++ src/com/mercury/beans/Stock.java
@@ -0,0 +1,67 @@
+package com.mercury.beans;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name="YFTS_STOCK")
+public class Stock implements Serializable{
+	private Integer sid;
+	private String symbol;
+	private String stockName;
+	private Set<OwnershipInfo> owns = new HashSet<OwnershipInfo>();
+	
+	@Id
+	@GeneratedValue(generator="stock_id_gen")
+	@GenericGenerator(name="stock_id_gen", strategy="increment")
+	@Column(name="STOCK_ID")
+	public Integer getSid() {
+		return sid;
+	}
+	public void setSid(Integer sid) {
+		this.sid = sid;
+	}
+	
+	@Column(name="SYMBOL")
+	public String getSymbol() {
+		return symbol;
+	}
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+	
+	@Column(name="STOCKNAME")
+	public String getStockName() {
+		return stockName;
+	}
+	public void setStockName(String stockName) {
+		this.stockName = stockName;
+	}
+	
+	@OneToMany(mappedBy="own.stock", cascade=CascadeType.ALL, fetch=FetchType.EAGER)
+	public Set<OwnershipInfo> getOwns() {
+		return owns;
+	}
+	public void setOwns(Set<OwnershipInfo> owns) {
+		this.owns = owns;
+	}
+	public void addOwns(OwnershipInfo osi){
+		owns.add(osi);
+	}
+	public void removeOwns(OwnershipInfo osi){
+		owns.remove(osi);
+	}
+}
--- src/com/mercury/beans/User.java
+++ src/com/mercury/beans/User.java
@@ -1,6 +1,24 @@
 package com.mercury.beans;
 
-public class User {
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name="YFTS_USER")
+public class User implements Serializable{
 	private int uid;
 	private String userName;
 	private String passWord;
@@ -10,77 +28,94 @@ public class User {
 	private int balance;
 	private String authority;
 	private int enabled;
-
+	private Set<OwnershipInfo> owns = new HashSet<OwnershipInfo>();
+	
+	@Id
+	@GeneratedValue(generator="user_id_gen")
+	@GenericGenerator(name="user_id_gen", strategy="increment")
+	@Column(name="USER_ID")
 	public int getUid() {
 		return uid;
 	}
-
 	public void setUid(int uid) {
 		this.uid = uid;
 	}
-
+	
+	@Column(name="USERNAME")
 	public String getUserName() {
 		return userName;
 	}
-
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-
+	
+	@Column(name="PASSWORD")
 	public String getPassWord() {
 		return passWord;
 	}
-
 	public void setPassWord(String passWord) {
 		this.passWord = passWord;
 	}
-
+	
+	@Column(name="EMAIL")
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
+	
+	@Column(name="FIRSTNAME")
 	public String getFirstName() {
 		return firstName;
 	}
-
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-
+	
+	@Column(name="LASTNAME")
 	public String getLastName() {
 		return lastName;
 	}
-
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-
+	
+	@Column(name="BALANCE")
 	public int getBalance() {
 		return balance;
 	}
-
 	public void setBalance(int balance) {
 		this.balance = balance;
 	}
-
+	
+	@Column(name="AUTHORITY")
 	public String getAuthority() {
 		return authority;
 	}
-
 	public void setAuthority(String authority) {
 		this.authority = authority;
 	}
-
+	
+	@Column(name="ENABLED")
 	public int getEnabled() {
 		return enabled;
 	}
-
 	public void setEnabled(int enabled) {
 		this.enabled = enabled;
 	}
 	
+	@OneToMany(mappedBy="own.user", cascade=CascadeType.ALL, fetch=FetchType.EAGER)
+	public Set<OwnershipInfo> getOwns() {
+		return owns;
+	}
+	public void setOwns(Set<OwnershipInfo> owns) {
+		this.owns = owns;
+	}
+	public void addOwns(OwnershipInfo osi){
+		owns.add(osi);
+	}
+	public void removeOwns(OwnershipInfo osi){
+		owns.remove(osi);
+	}
 }
