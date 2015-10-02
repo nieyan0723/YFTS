b6f85d05e775ddf7a355df62c915c9278a9f8c1f
shenpabk <shenpabk@yahoo.com>
2015-10-02 16:53:06 -0400

    add addMoney function

--- src/com/mercury/controllers/CustomerController.java
+++ src/com/mercury/controllers/CustomerController.java
@@ -1,13 +1,19 @@
 package com.mercury.controllers;
 
+import java.math.BigDecimal;
 import java.security.Principal;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+
+import javax.ws.rs.FormParam;
 
 import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +22,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.mercury.beans.Ownership;
 import com.mercury.beans.OwnershipInfo;
 import com.mercury.beans.StockInfo;
 import com.mercury.beans.User;
@@ -32,16 +39,25 @@ public class CustomerController {
 	@Autowired
 	private StockService ss;
 	
-	@RequestMapping(value="/portfolio", method=RequestMethod.GET)
-	public String portfolio() {
-		return "portfolio";
+//	@RequestMapping(value="/portfolio", method=RequestMethod.GET)
+//	public String portfolio() {
+//		return "portfolio";
+//	}
+	
+	@RequestMapping(value="/portfolio", method = RequestMethod.GET)
+	public ModelAndView portfolio(Principal principal) {
+		String username = principal.getName();
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("portfolio");
+		mav.addObject("balance", us.getBalance(username));
+		return mav;
 	}
 
 	@RequestMapping(value="/getOwnInfo", method=RequestMethod.GET)
 	@ResponseBody
 	public List<StockInfo> getPortfolio(Principal principal) {
 		String userName = principal.getName();
-		System.out.println(userName);
+//		System.out.println(userName);
 		List<OwnershipInfo> ownInfo = us.findOwnByUserName(userName);
 		List<StockInfo> list = new ArrayList<StockInfo>();
 		for(OwnershipInfo oi: ownInfo){
@@ -67,4 +83,17 @@ public class CustomerController {
 //		mav.addObject("userOwns", map);
 //		return mav;
 //	}
+	
+	@RequestMapping(value = "/addBal", method = RequestMethod.POST)
+	@ResponseBody
+	public int addBalance(@RequestBody Integer amount) {
+		String userName = SecurityContextHolder.getContext()
+				.getAuthentication().getName();
+		System.out.println(amount);
+		if(amount==null) amount=0;
+		
+		// if()
+		us.addBalance(userName, amount);
+		return us.getBalance(userName);
+	}
 }
